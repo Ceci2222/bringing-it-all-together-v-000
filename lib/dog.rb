c1d7982d95f50fb2986dbb57afd2d4b808e5c0a8
Marie Burns <mcburns2222@gmail.com>
2019-02-09 20:06:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,4 +25,37 @@ class Dog
 
     DB[:conn].execute(sql)  
   end
+  
+  def self.new_from_db(row)
+    new_dog = self.new(row[0], row[1], row[2])
+    new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogS WHERE name = ? LIMIT 1"
+    result = DB[:conn].execute(sql, name)
+    Dog.new(result[0], result[1], result[2])
+  end
+     
+  def update
+    sql = "UPDATE dogs SET  name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def save
+    if self.id
+      self.update 
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+        
+      
+      
 end
