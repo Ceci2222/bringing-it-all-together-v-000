2b1ad0fe1e443de2ec4f64344db8b7803f233c10
Marie Burns <mcburns2222@gmail.com>
2019-02-09 21:24:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,8 +78,8 @@ class Dog
   def self.find_or_create_by(hash)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash[:name], hash[:breed])
     dog = self.create(hash) if dog.empty?
-    binding.pry
     dog
+    binding.pry
   end
 
   
